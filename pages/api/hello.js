3f90c06c823645544ea6baef63822f45088a113c
Prashant Singh <v-singhpras@microsoft.com>
2022-02-02 14:26:10 +0530

    Adding api forwarding

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  if (req.method === 'POST') {
+    // Process a POST request
+    console.log(req.body)
+
+
+    axios.post('http://riviatech.eastus.cloudapp.azure.com:1337/answer', req.body)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+    res.statusCode = 200
+    res.json({ name: 'Data forwarded' })
+  } else {
+    // Handle any other HTTP method
+    res.statusCode = 200
+    res.json({ name: 'John Doe' })
+  }
+
 }
