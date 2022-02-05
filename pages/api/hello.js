c8fe24312f3cfab9ba1dbb9502975e1375effd6f
Prashant Singh <octanesingh@gmail.com>
2022-02-06 00:34:52 +0530

    Response checks

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,38 +2,34 @@
 import axios from "axios";
 
 export default (req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     // Process a POST request
-    console.log(req.body)
 
-
-    axios.post('http://riviatech.eastus.cloudapp.azure.com:1337/answer', {"name":`${req.body.name}`,"content":`${req.body.content}`})
-    .then(function (response) {
-      res.statusCode=200;
-      res.json({name:"Record successfully entered"})
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.json({error})
-    });
-
-
-  } else if (req.method === 'GET'){
-    axios.get('http://riviatech.eastus.cloudapp.azure.com:1337/question')
-    .then(function (response) {
-      res.json(response)
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.json({error})
-    });
-    
-  }
-  else {
+    axios
+      .post("http://riviatech.eastus.cloudapp.azure.com:1337/answer", {
+        name: `${req.body.name}`,
+        content: `${req.body.content}`,
+      })
+      .then(function (response) {
+        res.statusCode = 200;
+        res.json({ name: "Record successfully entered" });
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.json({ error });
+      });
+  } else if (req.method === "GET") {
+    axios
+      .get("http://riviatech.eastus.cloudapp.azure.com:1337/question")
+      .then(function (response) {
+        res.json(response.data);
+      })
+      .catch(function (error) {
+        res.json({ error });
+      });
+  } else {
     // Handle any other HTTP method
-    res.statusCode = 200
-    res.json({ name: 'Updated John Doe' })
+    res.statusCode = 200;
+    res.json({ name: "Updated John Doe" });
   }
-
-
-}
+};
